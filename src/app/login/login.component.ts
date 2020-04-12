b859c66bd51d200e883b7f90c3c93e073da0e229
Sam Kao <abc10324@hotmail.com>
2020-04-12 11:52:29 +0800

    Add user login connection to backend API

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -12,21 +14,45 @@ export class LoginComponent implements OnInit {
 
   user = {
     serailNo: 0,
-    username: '',
+    userName: '',
     password: ''
   }
 
-  constructor() { }
+  
+
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
   }
 
   login(formData :NgForm){
-    this.user.username = formData.value.username;
+    this.user.userName = formData.value.username;
     this.user.password = formData.value.password;
 
     console.log(this.user);
 
-    this.loginStatus.emit(this.user.username == 'abc10324' && this.user.password == 'abc9814016');
+    this.sendLoginRequest();
+  }
+
+  sendLoginRequest(){
+    this.httpClient.post('http://localhost:8080/login',this.user,httpOptions)
+               .subscribe( result => {
+                 console.log(result);
+                 console.log(result['status']);
+                 this.loginStatus.emit(result['status']);
+               },
+               error => {
+                 console.log(error);
+                 console.log(error['status']);
+                 console.log(error['message']);
+               });
   }
 }
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Authorization': '1qaz@WSX'
+  })
+};
